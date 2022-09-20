d0b44d91ff7e9aea877c9a1c1bcb63746b350692
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2022-09-20 17:33:45 -0300

    added h1 development

--- gitflow/src/App.js
+++ gitflow/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-	return <div className="App">ESTO ES UNA APP EN MI RAMA PRINCIPAL</div>;
+	return (
+		<div className="App">
+			ESTO ES UNA APP EN MI RAMA PRINCIPAL
+			<h1>Esto es development</h1>
+		</div>
+	);
 }
 
 export default App;
