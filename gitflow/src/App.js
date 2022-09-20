5c564d94a6ccebd846bb77fe98c961576c89e487
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2022-09-20 17:52:28 -0300

    finished cards

--- gitflow/src/App.js
+++ gitflow/src/App.js
@@ -4,11 +4,8 @@ function App() {
 	return (
 		<div className="App">
 			ESTO ES UNA APP EN MI RAMA PRINCIPAL
-<<<<<<< HEAD
 			<h2>Esto es erik</h2>
-=======
 			<h1>Esto es development</h1>
->>>>>>> d0b44d91ff7e9aea877c9a1c1bcb63746b350692
 		</div>
 	);
 }
---
+++ gitflow/src/components/Cards/Cards.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import s from './Cards.module.css';
+export default function Cards() {
+	return <div className={s.container}>ESTAS SON LAS FAMOSAS CARDS</div>;
+}
