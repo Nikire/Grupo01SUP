4968e8dfc2464b90eb7bda0820c7e1fb399abfc8
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2022-09-20 18:06:30 -0300

    footer terminado

--- gitflow/src/App.js
+++ gitflow/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-
+import Nav from './components/Nav/Nav';
+import Cards from './components/Cards/Cards';
 function App() {
 	return (
 		<div className="App">
 			ESTO ES UNA APP EN MI RAMA PRINCIPAL
+			<Nav />
 			<h2>Esto es erik</h2>
 			<h1>Esto es development</h1>
+			<Cards />
 		</div>
 	);
 }
