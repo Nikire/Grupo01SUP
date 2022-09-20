388aebc6999f04ab71bc79754da1d5e061bee97f
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2022-09-20 17:37:35 -0300

    added h2 erik

--- gitflow/src/App.js
+++ gitflow/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-	return <div className="App">ESTO ES UNA APP EN MI RAMA PRINCIPAL</div>;
+	return (
+		<div className="App">
+			ESTO ES UNA APP EN MI RAMA PRINCIPAL
+			<h2>Esto es erik</h2>
+		</div>
+	);
 }
 
 export default App;
